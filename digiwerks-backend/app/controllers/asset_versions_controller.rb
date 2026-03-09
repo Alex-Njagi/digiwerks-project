c472156aeae752d6a643d5ee4f6459688f4c0b00
-A <1992anjagi@gmail.com>
2026-03-09 09:35:27 +0300

    feat: AssetVersions controller successfully included

---
+++ digiwerks-backend/app/controllers/asset_versions_controller.rb
@@ -0,0 +1,51 @@
+class AssetVersionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :set_project_asset, only: [:index, :create]
+    before_action :set_version, only: [:show, :update, :destroy]
+
+    def index
+        versions = @asset.asset_versions
+        render json: versions
+    end
+
+    def show
+        render json: @version, include: :feedbacks
+    end
+
+    def create
+        version = @asset.asset_versions.build(version_params)
+        if version.save
+            render json: version, status: :created
+        else
+            render json: { errors: version.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @version.update(version_params)
+        render json: @version
+        else
+        render json: { errors: @version.errors }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @version.destroy
+        head :no_content
+    end
+
+    private
+
+    def set_project_asset
+        @asset = Asset.find(params[:asset_id])
+    end
+
+    def set_version
+        @version = AssetVersion.find(params[:id])
+    end
+
+    def version_params
+        params.permit(:version_number, :file_url, :change_notes)
+    end
+
+end
--- digiwerks-backend/app/models/asset_version.rb
+++ digiwerks-backend/app/models/asset_version.rb
@@ -2,7 +2,7 @@ class AssetVersion
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :asset_id, type: BSON::ObjectId
+  # field :asset_id, type: BSON::ObjectId
   field :version_number, type: Integer
   field :file_url, type: String
   field :change_notes, type: String
