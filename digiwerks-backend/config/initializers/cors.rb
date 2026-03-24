1e7254704d1aafad9ae9fd7aa47f3aecd17c7e74
-A <1992anjagi@gmail.com>
2026-03-24 09:54:25 +0300

    feat: update CORS origins and add Vercel configuration for rewrites

--- digiwerks-backend/config/initializers/cors.rb
+++ digiwerks-backend/config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://digiwerks-project.vercel.app/"
+    origins "http://localhost:5173",
+            "https://digiwerks-project.vercel.app"
+
     resource "*",
       headers: :any,
       methods: :any,
