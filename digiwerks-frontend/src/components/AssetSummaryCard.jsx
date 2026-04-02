a67d5eaa12d1609510d25e00d350f1e269e74d63
-A <1992anjagi@gmail.com>
2026-04-02 17:09:14 +0300

    feat: add delete functionality to AssetSummaryCard and update EditAsset page to use asset data

--- digiwerks-frontend/src/components/AssetSummaryCard.jsx
+++ digiwerks-frontend/src/components/AssetSummaryCard.jsx
@@ -10,6 +10,7 @@ import {
   Link
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useDeleteAsset } from "../hooks/useAssetHooks";
 
 export default function AssetSummaryCard({asset}) {
   const navigate = useNavigate();
@@ -17,11 +18,29 @@ export default function AssetSummaryCard({asset}) {
   const artistName = asset.project_stage?.project?.artist?.username 
   const projectId = asset.project_stage?.project?._id  
 
+  const assetId = asset._id
+  const { deleteAsset, loading: deleteLoading, error: deleteError } = useDeleteAsset();
+
+  const handleDelete = async () => {
+    const confirmed = window.confirm(
+        "Are you sure you want to delete this asset? This cannot be undone."
+    );
+    if (!confirmed) return;
+    try {
+        await deleteAsset(assetId);
+        alert("Your asset has been deleted successfully!");
+        navigate(`/projects/${projectId}`)
+    } catch (error) {
+        console.error(error);
+        alert("Failed to delete asset.");
+    }
+  };
+
   return (
     <Box
       bg="white"
       border="4px solid"
-      borderColor="brand.blue"
+      borderColor="brand.pink"
       borderRadius="xl"
       boxShadow="lg"
       p={8}
@@ -64,6 +83,35 @@ export default function AssetSummaryCard({asset}) {
             <Link onClick={()=>navigate(`/projects/${projectId}`)} color="brand.blue">{projectTitle}</Link>
           </HStack>
         </HStack>
+
+        <Flex justify="space-between" mt={2}>
+          <Button
+            size="sm"
+            bg="brand.pink"
+            color="white"
+            _hover={{ bg: "brand.blue" }}
+            // onClick={() => navigate("/project_asset/edit", { state: { asset } })}
+          >
+            Edit Asset
+          </Button>
+
+          <Button
+            size="sm"
+            bg="red.400"
+            color="white"
+            _hover={{ bg: "red.500" }}
+            onClick={handleDelete}
+            isLoading={deleteLoading}
+            loadingText="Deleting..."
+          >
+            Delete Asset
+          </Button>
+          {(deleteError) && (
+              <Text color="red.500" fontSize="sm">
+              {deleteError?.message}
+              </Text>
+          )} 
+        </Flex>
 
       </VStack>
     </Box>
